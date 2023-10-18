d4b6892bf9c43ca476173592d4341a830cfc8c6c
PedroTresmondi <pedro_tresmondi@hotmail.com>
2023-10-17 22:56:38 -0300

    adicionando toast no contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
@@ -6,10 +8,6 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
-
-// template_ym8ml9e
-// service_5z61nyc
-// N66AnGBGe4-sACxB0
 
 const Contact = () => {
   const formRef = useRef();
@@ -35,43 +33,45 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "Pedro Tresmondi",
-          from_email: form.email,
-          to_email: "pedro_tresmondi@hotmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Obrigado por entrar em contato! Responderei o mais rapido possivel.");
+    if (form.name.trim() === "" || form.email.trim() === "" || form.message.trim() === "") {
+      toast.error("Por favor, preencha todos os campos obrigatórios.");
+      setLoading(false);
+    } else {
+      emailjs
+        .send(
+          import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+          {
+            from_name: form.name,
+            to_name: "Pedro Tresmondi",
+            from_email: form.email,
+            to_email: "pedro_tresmondi@hotmail.com",
+            message: form.message,
+          },
+          import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then(
+          () => {
+            setLoading(false);
+            toast.success("Obrigado por entrar em contato! Responderei o mais rápido possível.");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Uhh, algo deu errado. Tente de novo por favor.");
-        }
-      );
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            console.error(error);
+            toast.error("Uhh, algo deu errado. Tente novamente, por favor.");
+            setLoading(false);
+          }
+        );
+    }
   };
 
   return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-    >
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
@@ -79,11 +79,7 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Entre em contato</p>
         <h3 className={styles.sectionHeadText}>Contato.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
+        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Seu Nome</span>
             <input
@@ -117,13 +113,16 @@ const Contact = () => {
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Enviando..." : "Enviar"}
-          </button>
+          <div>
+            <button
+              type='submit'
+              className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+              disabled={loading}
+            >
+              {loading ? "Enviando..." : "Enviar"}
+            </button>
+            <ToastContainer />
+          </div>
         </form>
       </motion.div>
 
